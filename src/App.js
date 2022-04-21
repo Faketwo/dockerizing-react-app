6d523ab93a1c1f3ead41da0926303b37b08161cb
jiaerhong <jiaerhong@ckex.tech>
2022-04-21 17:24:29 +0800

    dockerfile修改by生涫

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Dockerize React app.
+          Dockerize React apppppp.
         </p>
         <a
           className="App-link"
